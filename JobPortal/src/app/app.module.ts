e87548735237abdf6460b08cb2639423d14a6c99
amrokhaleed <amrokhalid110@gmail.com>
2024-05-02 14:24:25 +0300

    User Profile Page

--- JobPortal/src/app/app.module.ts
+++ JobPortal/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Library } from '@fortawesome/fontawesome-svg-core';
+import { faS } from '@fortawesome/free-solid-svg-icons';
+import { faR } from '@fortawesome/free-solid-svg-icons';
+import { FaConfig } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
+    FontAwesomeModule,
     BrowserModule,
     AppRoutingModule
   ],
