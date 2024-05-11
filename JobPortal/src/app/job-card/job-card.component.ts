89e1a933920bed66df9659e58508a8362ba5aa60
abdelrhman <abdelrhmankhaled537@gmail.com>
2024-05-11 16:31:31 +0300

    adding job details page

--- JobPortal/src/app/job-card/job-card.component.ts
+++ JobPortal/src/app/job-card/job-card.component.ts
@@ -14,15 +14,38 @@ export class JobCardComponent {
       title: 'UI UX Designer',
       company: 'Amazon',
       location: 'Jakarta, ID',
+      setting:'on-site',
       salary: '5000-8000',
+      description: 'Join Amazon\'s dynamic team in Jakarta as a UI/UX Designer, where you will have the opportunity to shape the digital experiences of millions of customers worldwide. As a UI/UX Designer, you will collaborate with cross-functional teams to conceptualize and design innovative user interfaces and experiences that elevate the Amazon brand and drive user engagement. You will utilize your creativity, user-centered design principles, and technical expertise to deliver intuitive and visually appealing solutions across web and mobile platforms. This role offers a unique chance to work on challenging projects, contribute to Amazon\'s success, and grow your career in a fast-paced and inclusive environment.',
       type: 'Full-time',
       date: '3 July 2023',
       imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ffreebiesupply.com%2Flogos%2Famazon-dark-logo%2F&psig=AOvVaw1VdyhlJCHOeJu-ydc-zRUF&ust=1715288247690000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIjLv-f4_oUDFQAAAAAdAAAAABAZ',
-      responsibilities: 'Responsibilities of UI UX Designer...',
-      qualifications: 'Qualifications of UI UX Designer...',
+      responsibilities: [
+        'Collaborate with product management and engineering teams to define and implement innovative solutions for the product direction, visuals, and experience.',
+        'Execute all visual design stages from concept to final hand-off to engineering.',
+        'Conceptualize original ideas that bring simplicity and user friendliness to complex design roadblocks.',
+        'Create wireframes, storyboards, user flows, process flows, and site maps to effectively communicate interaction and design ideas.',
+        'Present and defend designs and key milestone deliverables to peers and executive level stakeholders.',
+        'Conduct user research and evaluate user feedback.',
+        'Establish and promote design guidelines, best practices, and standards.',
+        'Stay updated on emerging technologies and industry trends.'
+    ],
+    qualifications: [
+        'Bachelor\'s degree in Design, Fine Arts, Computer Science, or a related field.',
+        'Proven UI/UX design experience with a strong portfolio.',
+        'Proficiency in design and prototyping tools such as Sketch, Adobe XD, Figma, or similar.',
+        'Experience in creating wireframes, storyboards, user flows, process flows, and site maps.',
+        'Ability to present your designs and sell your solutions to various stakeholders.',
+        'Up-to-date with the latest UI/UX trends, techniques, and technologies.',
+        'Excellent visual design skills with sensitivity to user-system interaction.',
+        'Ability to solve problems creatively and effectively.',
+        'Experience working in an Agile/Scrum development process is a plus.',
+        'Excellent communication, interpersonal, and collaboration skills.'
+    ],
       benefits: 'Benefits of UI UX Designer...',
       howToApply: 'How to apply for UI UX Designer...'
     }
+    
   ];
 
   constructor() { }
